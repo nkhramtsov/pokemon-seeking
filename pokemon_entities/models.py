4c4c8ea185a37d8d01d511d37877e2358e6a91fc
nkhramtsov <nkramtsov@gmail.com>
2022-08-31 21:05:27 +0500

    Show pokemons on mainpage

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(upload_to='pokemon_images', blank=True, null=True)
 
     def __str__(self):
         return f'{self.title}'
@@ -12,8 +12,8 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
 
-    latitude = models.FloatField(null=True)
-    longitude = models.FloatField(null=True)
+    lat = models.FloatField(null=True)
+    lon = models.FloatField(null=True)
 
     appeared_at = models.DateTimeField(null=True)
     disappeared_at = models.DateTimeField(null=True)
